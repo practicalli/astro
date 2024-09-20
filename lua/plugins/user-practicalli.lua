3909419787a1ef45d8719973c3e6bac47c962d85
Greg Warner <gdwarner@Gmail.com>
2024-09-20 13:33:45 -0600

    Remove telescope projects
    No longer works since a project plugin is no longer used.  Related discussion: https://clojurians.slack.com/archives/CJTRRQ857/p1718834288549289
    
    Signed-off-by: Greg Warner <gdwarner@Gmail.com>

--- lua/plugins/user-practicalli.lua
+++ lua/plugins/user-practicalli.lua
@@ -265,9 +265,6 @@ return {
             desc = "Grep Word",
           },
 
-          -- Projects
-          ["<Leader>fp"] = { "<cmd>Telescope projects<cr>", desc = "Projects" },
-
           -- Editing
           ["zZ"] = { "<cmd>ZenMode<cr>", desc = " Zen mode" },
 
